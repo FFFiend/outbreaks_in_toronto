3089468c06e56bdd54a333686871de8afbbc0c66
Owais <owais.zahid@mail.utoronto.ca>
2024-01-23 04:07:38 +0000

    updated simulate data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -5,40 +5,46 @@
 # Date: 22 January 2024
 # Prerequisites: Know where to obtain outbreak data
 
-#### Workspace setup ####
-library("tidyverse")
 
-### Setting seed ###
-set.seed(123)
+### Workspace setup ###
+library(tidyverse)
 
-### Setting start end months of 2023 ###
-### and the number of outbreaks to match the dataset size ###
+### Setting prime number seed ###
+set.seed(479)
+
+# Step 1: Generate random data
+# choosing 1067 as the number of entries as that is the dataset's size
+n_entries <- 1067
 start_date <- as.Date("2023-01-01")
 end_date <- as.Date("2023-12-31")
-num_outbreaks <- 1067
 
-outbreak_data <- data.frame(
-  Date = sample(seq(start_date, end_date, by = "day"), num_outbreaks, replace = TRUE),
-  Institution = paste("Institution", seq(1, num_outbreaks)),
-  Causative_Agent = sample(c(
-    "COVID-19", "Parainfluenza", "Respiratory syncytial virus", "Coronavirus*",
-    "Metapneumovirus", "Influenza A (Not subtyped)", "Pending",
-    "Unable to identify", "Norovirus", "Influenza A (H3)", "Rhinovirus",
-    "Group B Streptococcal disease (neonatal)", "Influenza A (H1N1)",
-    "Enterovirus/Rhinovirus", "Enterovirus", "Parainfluenza PIV III",
-    "Streptococcus pyogenes", "CPE Enterobacter unspecified (NDM)",
-    "CPE Unspecified (KPC)"
-  ), num_outbreaks, replace = TRUE)
-)
+dates <- sample(seq(start_date, end_date, by = "day"), n_entries, replace = TRUE)
+institutions <- sample(letters, n_entries, replace = TRUE)
+causative_agents <- sample(c("COVID-19", "Parainfluenza", 
+                             "Respiratory syncytial virus", 
+                             "Coronavirus*", "Metapneumovirus", 
+                             "Influenza A (Not subtyped)", 
+                             "Pending", "Unable to identify",
+                             "Norovirus", "Influenza A (H3)", 
+                             "Rhinovirus", "Group B Streptococcal disease
+                             (neonatal)", 
+                             "Influenza A (H1N1)", 
+                             "Enterovirus/Rhinovirus", 
+                             "Enterovirus", 
+                             "Parainfluenza PIV III", "Streptococcus pyogenes",
+                             "CPE Enterobacter unspecified (NDM)", 
+                             "CPE Unspecified (KPC)"), 
+                           n_entries, replace = TRUE)
 
-# Step 2: Create a histogram with stacked bars
-library(ggplot2)
+data <- data.frame(Date = dates, Institution = institutions, CausativeAgent = causative_agents)
 
-ggplot(outbreak_data, aes(x = Date, fill = Causative_Agent)) +
-  geom_bar(position = "stack") +
-  labs(title = "Outbreaks Over 2023",
+# Step 2: Create histogram
+data %>%
+  group_by(Date, CausativeAgent) %>%
+  summarise(Count = n()) %>%
+  ggplot(aes(x = Date, y = Count, fill = CausativeAgent)) +
+  geom_bar(stat = "identity", position = "stack") +
+  labs(title = "Outbreaks Over the Year 2023",
        x = "Date",
-       y = "Total Cases",
-       fill = "Causative Agent") +
+       y = "Total Number of Cases") +
   theme_minimal()
-
